6f91af3bdae17f251d67a1258276f5324d883600
unknown <satyabhusan9853@gmail.com>
2024-01-28 21:38:10 +0530

    Second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Satya9853/puppy
+
+go 1.21.5
---
+++ puppy.go
@@ -0,0 +1,11 @@
+package puppy
+
+import "fmt"
+
+func Bark() {
+	fmt.Println("Woof..!")
+}
+
+func Barks(){
+	fmt.Println("Woof...Woof...Woof")
+}
